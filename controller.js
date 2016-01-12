19a8e6cd50d2a4e9886fa87c18866c6b5a6ce6fd
ericsvendsen <esvendsen@gmail.com>
2016-01-12 16:42:40 -0500

    updating url

--- controller.js
+++ controller.js
@@ -32,13 +32,13 @@ exports.importRaster = {
                 //var cmd = 'raster2pgsql -s 4326 -I -C -M ' + path + ' -F public.products | psql -d scale';
                 async.series([
                     function () {
-                        exec('curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>scale</name></workspace>" http://localhost/geoserver/rest/workspaces/');
+                        exec('curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>scale</name></workspace>" http://127.0.0.1/geoserver/rest/workspaces/');
                     },
                     function () {
                         exec('sudo mv uploads/' + name + ' ~/dataviz-nov2015/geoserver_data/data');
                     },
                     function () {
-                        var cmd = 'curl -v -u admin:geoserver -XPUT -H "Content-type: application/octet-stream" --data-binary @' + name + ' http://localhost/geoserver/rest/workspaces/scale/datastores/products/' + name;
+                        var cmd = 'curl -v -u admin:geoserver -XPUT -H "Content-type: application/octet-stream" --data-binary @' + name + ' http://127.0.0.1/geoserver/rest/workspaces/scale/datastores/products/' + name;
                         exec(cmd, { maxBuffer: 314572800 }, function (error, stderr, stdout) {
                             if (error) {
                                 reply(boom.expectationFailed(error, stderr));
