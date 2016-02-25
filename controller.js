7c0e4000d9358839827fd563661fd13cc05a7e50
ericsvendsen <esvendsen@gmail.com>
2016-02-25 10:34:37 -0500

    added endpoints for creating/updating mosaic

--- controller.js
+++ controller.js
@@ -92,3 +92,100 @@ exports.importRaster = {
         }
     }
 };
+
+exports.createMosaic = {
+    payload: {
+        output: 'data',
+        parse: true,
+        allow: 'text/plain',
+        maxBytes: 314572800 // 300mb
+    },
+    handler: function (request, reply) {
+        var data = request.payload;
+
+        if (data.layer) {
+            var cmd = '';
+            async.series([
+                // create workspace
+                function (callback) {
+                    cmd = 'curl -v -u admin:geoserver -XPOST -H "Content-type: text/xml" -d "<workspace><name>scale</name></workspace>" http://localhost/geoserver/rest/workspaces'
+                    exec(cmd, function (error, stderr, stdout) {
+                        if (error) {
+                            reply(boom.expectationFailed(error, stderr));
+                        } else {
+                            callback();
+                        }
+                    });
+                },
+                // create datastore
+                function (callback) {
+                    cmd = 'curl -v -u admin:geoserver -XPOST -H "Content-Type: text/xml" -d "<coverageStore><name>' + data.layer + '</name><workspace>scale</workspace><enabled>true</enabled></coverageStore>" http://localhost/geoserver/rest/workspaces/scale/coveragestores';
+                    exec(cmd, function (error, stderr, stdout) {
+                        if (error) {
+                            reply(boom.expectationFailed(error, stderr));
+                        } else {
+                            callback();
+                        }
+                    });
+                }
+            ],
+            function (err, results) {
+                reply(boom.expectationFailed(err));
+            });
+        }
+    }
+};
+
+exports.updateMosaic = {
+    payload: {
+        output: 'stream',
+        parse: true,
+        allow: 'multipart/form-data',
+        maxBytes: 314572800 // 300mb
+    },
+    handler: function (request, reply) {
+        var data = request.payload;
+
+        if (data.file) {
+            var name = data.file.hapi.filename,
+                path = __dirname + '/uploads/' + name,
+                file = fs.createWriteStream(path),
+                mosaic = data.mosaic;
+
+            file.on('error', function (err) {
+                console.error(err);
+                reply(boom.expectationFailed(err));
+            });
+
+            data.file.pipe(file);
+
+            data.file.on('end', function () {
+                var cmd = '';
+                async.series([
+                    // upload file
+                    function (callback) {
+                        cmd = 'curl -v -u admin:geoserver -XPUT -H "Content-type: application/zip" --data-binary @uploads/' + name + ' http://localhost/geoserver/rest/workspaces/scale/coveragestores/' + data.mosaic + '/external.imagemosaic';
+                        exec(cmd, { maxBuffer: 314572800 }, function (error, stderr, stdout) {
+                            if (error) {
+                                reply(boom.expectationFailed(error, stderr));
+                            } else {
+                                callback(null, stdout);
+                            }
+                        });
+                    }
+                ],
+                function (err, results) {
+                    // cleanup
+                    cmd = 'sudo rm uploads/' + name;
+                    exec(cmd, function (error, stderr, stdout) {
+                        if (error) {
+                            reply(boom.expectationFailed(error, stderr));
+                        } else {
+                            reply(JSON.stringify(results));
+                        }
+                    });
+                });
+            }
+        }
+    }
+};
--- routes.js
+++ routes.js
@@ -5,5 +5,15 @@ exports.endpoints = [
         method: 'POST',
         path: '/importraster',
         config: Controller.importRaster
+    },
+    {
+        method: 'POST',
+        path: '/mosaic',
+        config: Controller.createMosaic
+    },
+    {
+        method: 'PUT',
+        path: '/mosaic',
+        config: Controller.updateMosaic
     }
 ];
